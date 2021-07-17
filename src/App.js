567bbb74401cbb8b20ecfb23a1d1d88c7d47e71c
Fredrik <fredrikf2000@gmail.com>
2021-07-17 17:32:34 +0200

    finished products section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 
 import { BrowserRouter as Router } from "react-router-dom";
 import { GlobalStyle } from "./globalStyles";
+import Products from "./components/Products";
+import { productData } from "./components/Products/data";
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
+      <Products heading="Choose your favorite" data={productData} />
     </Router>
   );
 }
